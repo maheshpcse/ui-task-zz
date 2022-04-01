ad9d9edee802448befed4c9723c57453ad44bf47
Mahesh Pachapalam <maheshmahi1599@gmail.com>
2022-04-01 20:02:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ui-task-zz'));
+app.use(express.static(__dirname + '/dist/ui-task'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ui-task-zz/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ui-task/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
